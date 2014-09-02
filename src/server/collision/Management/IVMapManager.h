0094f5a281ff8e083b2ee32def3a41fec339f302
Keany <daniel.totka@gmail.com>
2014-09-02 11:35:10 +0200

    Revert "Revert "UPDATE collision""
    This reverts commit 5ac4126b5bb4ce12ecf8e7b8cec58f46f1fd90b8.

--- src/server/collision/Management/IVMapManager.h
+++ src/server/collision/Management/IVMapManager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -35,7 +35,7 @@ namespace VMAP
     {
         VMAP_LOAD_RESULT_ERROR,
         VMAP_LOAD_RESULT_OK,
-        VMAP_LOAD_RESULT_IGNORED,
+        VMAP_LOAD_RESULT_IGNORED
     };
 
     #define VMAP_INVALID_HEIGHT       -100000.0f            // for check
--- src/server/collision/Models/GameObjectModel.h
+++ src/server/collision/Models/GameObjectModel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -60,6 +60,8 @@ public:
     /**    Enables\disables collision. */
     void disable() { phasemask = 0;}
     void enable(uint32 ph_mask) { phasemask = ph_mask;}
+
+    bool isEnabled() const {return phasemask != 0;}
 
     bool intersectRay(const G3D::Ray& Ray, float& MaxDist, bool StopAtFirstHit, uint32 ph_mask) const;
 
